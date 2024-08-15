455a9268e06eb44c0f5e1a6360acc4485c87daec
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 21:28:05 -0300

    Aula 10

---
+++ Semestre 2/Aula 10/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./componentes/Header.jsx"
+import MainInfo from "./componentes/MainInfo.jsx"
+import Footer from "./componentes/Footer.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <MainInfo/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Semestre 2/Aula 10/vite-project/src/componentes/MainInfo.jsx
@@ -0,0 +1,42 @@
+import "../../src/index.css"
+
+function MainInfo() {
+    return (
+        <div class="mainInfo">
+         <section id="info">
+            <div className="personal-info">
+                <h2>Informações Pessoais</h2>
+                <p>Nome: João</p>
+                <p>Email: joao@example.com</p>
+                <p>Telefone: (00) 12345-6789</p>
+            </div>
+            <div className="skills">
+                <h2>Habilidades</h2>
+                <div class="skill-card">
+                    <h3>HTML</h3>
+                    <p>Nível: Avançado</p>
+                </div>
+                <div className="skill-card">
+                    <h3>CSS</h3>
+                    <p>Nível: Intermediário</p>
+                </div>
+                <div className="skill-card">
+                    <h3>JavaScript</h3>
+                    <p>Nível: Básico</p>
+                </div>
+            </div>
+        </section>
+        <section id="contact">
+        <h2>Entre em contato</h2>
+            <form>
+                <input type="text" placeholder="Nome"/>
+                <input type="email" placeholder="Email"/>
+                <textarea placeholder="Mensagem"></textarea>
+                <button type="submit">Enviar</button>
+            </form>
+            </section>
+        </div>
+    );
+}
+
+export default MainInfo;
